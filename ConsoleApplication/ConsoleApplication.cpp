59ac6307f641cab3d26bb394702baa7d7114af36
mkuliberda <m.kuliberda@s4t.com.pl>
2020-10-22 19:05:21 +0200

    Add project files.

---
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,58 @@
+// ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "main.h"
+#include <bitset>
+#include <stdlib.h>     //for using the function sleep
+#include <windows.h>
+
+
+
+void decorator_test(void) {
+	std::string name = "Pelargonia";
+	uint8_t id = 0;
+	PlantInterface *anPlantWithDMAMoistureSensor = new PlantWithDMAMoistureSensor(new Plant(name, 0), 3.3, 4095);
+	cout << anPlantWithDMAMoistureSensor->getMoisturePercent() << endl;
+	anPlantWithDMAMoistureSensor->updateRaw((uint16_t)3);
+	cout << anPlantWithDMAMoistureSensor->getMoisturePercent() << endl;
+	cout << anPlantWithDMAMoistureSensor->getName() << endl;
+	cout << anPlantWithDMAMoistureSensor->getId() << endl;
+
+	std::vector< std::unique_ptr<PlantInterface>> vPlants;
+	vPlants.emplace_back(std::unique_ptr<PlantInterface>(new PlantWithDMAMoistureSensor(new Plant("Pelargonia", 0), 3.0, 4095)));
+	cout<< "vPlants size: "<< vPlants.size() << " capacity: "<< vPlants.capacity()<<endl;
+	vPlants.emplace_back(std::unique_ptr<PlantInterface>(new PlantWithDMAMoistureSensor(new Plant("Kroton", 0), 3.0, 4095)));
+	cout << "vPlants size: " << vPlants.size() << " capacity: " << vPlants.capacity() << endl;
+	vPlants.emplace_back(std::unique_ptr<PlantInterface>(new PlantWithDMAMoistureSensor(new Plant("Surfinia", 0), 3.0, 4095)));
+	cout << "vPlants size: " << vPlants.size() << " capacity: " << vPlants.capacity() << endl;
+
+
+	//plants.emplace_back()
+	delete anPlantWithDMAMoistureSensor;
+
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	while (1) {
+		decorator_test();
+		Sleep(1000);
+	}
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
